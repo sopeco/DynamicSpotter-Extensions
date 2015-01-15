73f6dd6a9712cd7368f1350b17d0142b2f6f133d
Denis Knoepfle <denis.knoepfle@sap.com>
2015-01-15 13:56:52 +0100

    Add missing parameters to the RampExtension
    Change-Id: I8d4fb06052ea9c30ab478cd4ea6ba86f8d5180e3
    Signed-off-by: Denis Knoepfle <denis.knoepfle@sap.com>

--- org.spotter.ext.detection.ramp/src/org/spotter/ext/detection/ramp/RampExtension.java
+++ org.spotter.ext.detection.ramp/src/org/spotter/ext/detection/ramp/RampExtension.java
@@ -16,6 +16,7 @@
 package org.spotter.ext.detection.ramp;
 
 import org.lpe.common.config.ConfigParameterDescription;
+import org.lpe.common.util.LpeSupportedTypes;
 import org.spotter.core.detection.AbstractDetectionExtension;
 import org.spotter.core.detection.IDetectionController;
 
@@ -40,9 +41,55 @@ public class RampExtension extends AbstractDetectionExtension {
 	protected static final int REQUIRED_SIGNIFICANT_STEPS_DEFAULT = 2;
 	protected static final double MAX_CPU_UTILIZATION_DEFAULT = 0.9; //[0-1] (percentage)
 	
+	private static final int STIMULATION_PHASE_MIN_DURATION = 10; //[Sec]
+	
 	@Override
 	public String getName() {
 		return "Ramp";
+	}
+	
+	private ConfigParameterDescription createStimulationPhaseDurationParameter() {
+		ConfigParameterDescription parameter = new ConfigParameterDescription(KEY_WARMUP_PHASE_DURATION, LpeSupportedTypes.Integer);
+		parameter.setDefaultValue(String.valueOf(STIMULATION_PHASE_DURATION_DEFAULT));
+		parameter.setRange(String.valueOf(STIMULATION_PHASE_MIN_DURATION), String.valueOf(Integer.MAX_VALUE));
+		parameter.setDescription("The duration of the stimulation phase.");
+		return parameter;
+	}
+	
+	private ConfigParameterDescription createNumExperimentsParameter() {
+		ConfigParameterDescription parameter = new ConfigParameterDescription(KEY_EXPERIMENT_STEPS, LpeSupportedTypes.Integer);
+		parameter.setDefaultValue(String.valueOf(EXPERIMENT_STEPS_DEFAULT));
+		parameter.setRange(String.valueOf(2), String.valueOf(Integer.MAX_VALUE));
+		parameter.setDescription("Number of experiments to execute with "
+				+ "different number of users between 1 and max number of users.");
+		return parameter;
+	}
+	
+	private ConfigParameterDescription createRequiredSignificanceLevelParameter() {
+		ConfigParameterDescription parameter = new ConfigParameterDescription(KEY_REQUIRED_SIGNIFICANCE_LEVEL, LpeSupportedTypes.Double);
+		parameter.setDefaultValue(String.valueOf(REQUIRED_SIGNIFICANCE_LEVEL_DEFAULT));
+		parameter.setRange(String.valueOf(0.0), String.valueOf(1.0));
+		parameter.setDescription("This parameter defines the confidence level to be reached "
+				+ "in order to recognize a significant difference when comparing "
+				+ "two response time samples with the t-test.");
+		return parameter;
+	}
+	
+	private ConfigParameterDescription createRequiredSignificantStepsParameter() {
+		ConfigParameterDescription parameter = new ConfigParameterDescription(KEY_REQUIRED_SIGNIFICANT_STEPS, LpeSupportedTypes.Integer);
+		parameter.setDefaultValue(String.valueOf(REQUIRED_SIGNIFICANT_STEPS_DEFAULT));
+		parameter.setRange(String.valueOf(2), String.valueOf(Integer.MAX_VALUE));
+		parameter.setDescription("This parameter specifies the number of steps between experiments "
+				+ "required to show a significant increase in order to detect a Ramp.");
+		return parameter;
+	}
+	
+	private ConfigParameterDescription createCpuThresholdParameter() {
+		ConfigParameterDescription parameter = new ConfigParameterDescription(KEY_CPU_UTILIZATION_THRESHOLD, LpeSupportedTypes.Double);
+		parameter.setDefaultValue(String.valueOf(MAX_CPU_UTILIZATION_DEFAULT));
+		parameter.setRange(String.valueOf(0.0), String.valueOf(1.0));
+		parameter.setDescription("Defines the CPU utilization threshold, when a system is considered as overutilized.");
+		return parameter;
 	}
 
 	@Override
@@ -53,5 +100,10 @@ public class RampExtension extends AbstractDetectionExtension {
 	@Override
 	protected void initializeConfigurationParameters() {
 		addConfigParameter(ConfigParameterDescription.createExtensionDescription(EXTENSION_DESCRIPTION));
+		addConfigParameter(createStimulationPhaseDurationParameter());
+		addConfigParameter(createNumExperimentsParameter());
+		addConfigParameter(createRequiredSignificanceLevelParameter());
+		addConfigParameter(createRequiredSignificantStepsParameter());
+		addConfigParameter(createCpuThresholdParameter());
 	}
 }
