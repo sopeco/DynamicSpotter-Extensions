d4f4bbca2aad9e340eab5dc2a5e18d0630eb0ce5
Alexander Wert <alexander.wert@sap.com>
2015-02-09 09:35:39 +0100

    added DBCongestion Heuristic
    Change-Id: I756055fbdb873e936667ef7d1e36d0f448ce06af
    Signed-off-by: Alexander Wert <alexander.wert@sap.com>

---
+++ org.spotter.ext.detection.collection/src/org/spotter/ext/detection/dbcongestion/DBCongestionDetectionController.java
@@ -0,0 +1,314 @@
+package org.spotter.ext.detection.dbcongestion;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.aim.api.exceptions.InstrumentationException;
+import org.aim.api.exceptions.MeasurementException;
+import org.aim.api.measurement.dataset.Dataset;
+import org.aim.api.measurement.dataset.DatasetCollection;
+import org.aim.api.measurement.dataset.ParameterSelection;
+import org.aim.artifacts.records.CPUUtilizationRecord;
+import org.aim.artifacts.records.DBStatisticsRecrod;
+import org.aim.artifacts.sampler.CPUSampler;
+import org.aim.description.InstrumentationDescription;
+import org.aim.description.builder.InstrumentationDescriptionBuilder;
+import org.lpe.common.config.ConfigParameterDescription;
+import org.lpe.common.config.GlobalConfiguration;
+import org.lpe.common.extension.IExtension;
+import org.lpe.common.util.LpeNumericUtils;
+import org.lpe.common.util.NumericPairList;
+import org.spotter.core.detection.AbstractDetectionController;
+import org.spotter.core.detection.IDetectionController;
+import org.spotter.core.detection.IExperimentReuser;
+import org.spotter.exceptions.WorkloadException;
+import org.spotter.ext.detection.utils.AnalysisChartBuilder;
+import org.spotter.shared.configuration.ConfigKeys;
+import org.spotter.shared.result.model.SpotterResult;
+
+public class DBCongestionDetectionController extends AbstractDetectionController implements IExperimentReuser {
+
+	private int requiredSignificantSteps;
+	private double requiredSignificanceLevel;
+	private double cpuThreshold;
+	private int experimentSteps;
+	private boolean qtStrategy = false;
+
+	public DBCongestionDetectionController(IExtension<IDetectionController> provider) {
+		super(provider);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void loadProperties() {
+		String experimentStepsStr = getProblemDetectionConfiguration().getProperty(
+				DBCongestionExtension.EXPERIMENT_STEPS_KEY);
+		experimentSteps = experimentStepsStr != null ? Integer.parseInt(experimentStepsStr)
+				: DBCongestionExtension.EXPERIMENT_STEPS_DEFAULT;
+
+		String requiredSignificantStepsStr = getProblemDetectionConfiguration().getProperty(
+				DBCongestionExtension.REQUIRED_SIGNIFICANT_STEPS_KEY);
+		requiredSignificantSteps = requiredSignificantStepsStr != null ? Integer.parseInt(requiredSignificantStepsStr)
+				: DBCongestionExtension.REQUIRED_SIGNIFICANT_STEPS_DEFAULT;
+
+		String requiredConfidenceLevelStr = getProblemDetectionConfiguration().getProperty(
+				DBCongestionExtension.REQUIRED_CONFIDENCE_LEVEL_KEY);
+		requiredSignificanceLevel = 1.0 - (requiredConfidenceLevelStr != null ? Double
+				.parseDouble(requiredConfidenceLevelStr) : DBCongestionExtension.REQUIRED_CONFIDENCE_LEVEL_DEFAULT);
+
+		String cpuThresholdStr = getProblemDetectionConfiguration()
+				.getProperty(DBCongestionExtension.CPU_THRESHOLD_KEY);
+		cpuThreshold = cpuThresholdStr != null ? Double.parseDouble(cpuThresholdStr)
+				: DBCongestionExtension.CPU_THRESHOLD_DEFAULT;
+
+		String tmpStrategy = getProblemDetectionConfiguration().getProperty(
+				DBCongestionExtension.DETECTION_STRATEGY_KEY);
+		switch (tmpStrategy) {
+		case DBCongestionExtension.THRESHOLD_STRATEGY:
+			qtStrategy = false;
+			break;
+		case DBCongestionExtension.QT_STRATEGY:
+			qtStrategy = true;
+			break;
+		default:
+			qtStrategy = false;
+			break;
+		}
+	}
+
+	@Override
+	public long getExperimentSeriesDuration() {
+		return 0;
+	}
+
+	@Override
+	public void executeExperiments() throws InstrumentationException, MeasurementException, WorkloadException {
+		executeDefaultExperimentSeries(this, experimentSteps, getInstrumentationDescription());
+	}
+
+	@Override
+	protected SpotterResult analyze(DatasetCollection data) {
+		SpotterResult result = new SpotterResult();
+
+		Dataset dbDataset = data.getDataSet(DBStatisticsRecrod.class);
+
+		for (String dbId : dbDataset.getValueSet(DBStatisticsRecrod.PAR_PROCESS_ID, String.class)) {
+			List<Integer> sortedNumUsersList = new ArrayList<Integer>(dbDataset.getValueSet(
+					AbstractDetectionController.NUMBER_OF_USERS_KEY, Integer.class));
+			boolean detected = analyzeDBStatistics(dbDataset, dbId, sortedNumUsersList, result);
+			if (detected) {
+				result.setDetected(true);
+				result.addMessage("Database overhead detected on database " + dbId
+						+ " due to increasing locking times!");
+			}
+		}
+
+		Dataset dbUtilDataset = data.getDataSet(CPUUtilizationRecord.class);
+		String dbHostStr = GlobalConfiguration.getInstance().getProperty(ConfigKeys.SYSTEM_NODE_ROLE_DB);
+		List<String> dbHosts = new ArrayList<>();
+		for (String host : dbHostStr.split(ConfigParameterDescription.LIST_VALUE_SEPARATOR)) {
+			dbHosts.add(host);
+		}
+		for (String processID : dbUtilDataset.getValueSet(CPUUtilizationRecord.PAR_PROCESS_ID, String.class)) {
+			boolean detected = analyzeCPUUtilization(dbUtilDataset, dbHosts, processID, result);
+			if (detected) {
+				result.setDetected(true);
+				result.addMessage("Database overhead detected on database " + processID
+						+ " due to high CPU utilization on database!");
+			}
+		}
+
+		return result;
+	}
+
+	private boolean analyzeCPUUtilization(Dataset dbUtilDataset, List<String> dbHosts, String processID,
+			SpotterResult result) {
+		boolean detected = false;
+		NumericPairList<Integer, Double> chartDataUtils = new NumericPairList<>();
+
+		ParameterSelection dbNodeelection = new ParameterSelection().select(CPUUtilizationRecord.PAR_PROCESS_ID,
+				processID);
+		Dataset dataset = dbNodeelection.applyTo(dbUtilDataset);
+		Map<String, Integer> mapNumCores = getNumberOfCPUCores(dataset);
+		for (Integer numUsers : dbUtilDataset.getValueSet(AbstractDetectionController.NUMBER_OF_USERS_KEY,
+				Integer.class)) {
+			ParameterSelection usersSelection = new ParameterSelection()
+					.select(AbstractDetectionController.NUMBER_OF_USERS_KEY, numUsers)
+					.select(CPUUtilizationRecord.PAR_PROCESS_ID, processID)
+					.select(CPUUtilizationRecord.PAR_CPU_ID, CPUUtilizationRecord.RES_CPU_AGGREGATED);
+			Dataset tmpDataset = usersSelection.applyTo(dbUtilDataset);
+
+			List<Double> cpuUtils = new ArrayList<>();
+			for (CPUUtilizationRecord rec : tmpDataset.getRecords(CPUUtilizationRecord.class)) {
+				boolean isDBRecord = false;
+				for (String dbHost : dbHosts) {
+					if (rec.getProcessId().contains(dbHost)) {
+						isDBRecord = true;
+						break;
+					}
+				}
+
+				if (isDBRecord) {
+					cpuUtils.add(rec.getUtilization());
+				}
+			}
+
+			double meanCPUUtil = LpeNumericUtils.average(cpuUtils);
+
+			double actualThreshold = cpuThreshold;
+			if (qtStrategy) {
+				actualThreshold = LpeNumericUtils.getUtilizationForResponseTimeFactorQT(5, mapNumCores.get(processID));
+			} else {
+				actualThreshold = cpuThreshold;
+			}
+			if (meanCPUUtil >= actualThreshold) {
+				detected = true;
+			}
+			chartDataUtils.add(numUsers, meanCPUUtil);
+		}
+
+		AnalysisChartBuilder chartBuilder = new AnalysisChartBuilder();
+		chartBuilder.startChart(processID, "Num Users", "CPU Utilization [%]");
+		chartBuilder.addScatterSeries(chartDataUtils, "Utilization");
+		chartBuilder.addHorizontalLine(cpuThreshold, "Threshold");
+		getResultManager().storeImageChartResource(chartBuilder.build(), "DB-CPU Utilization", result);
+
+		return detected;
+	}
+
+	private boolean analyzeDBStatistics(Dataset dbDataset, String dbId, List<Integer> sortedNumUsersList,
+			SpotterResult result) {
+		Collections.sort(sortedNumUsersList);
+		int prevNumUsers = -1;
+		int firstSignificantNumUsers = -1;
+		int significantSteps = 0;
+		int minNumUsers = sortedNumUsersList.get(0);
+		NumericPairList<Integer, Double> rawData = new NumericPairList<>();
+		NumericPairList<Integer, Double> means = new NumericPairList<>();
+		List<Number> ci = new ArrayList<>();
+		List<Double> waitTimesPerLock_prev = null;
+		for (Integer numUsers : sortedNumUsersList) {
+			Dataset tmpDataset = ParameterSelection.newSelection().select(NUMBER_OF_USERS_KEY, numUsers)
+					.select(DBStatisticsRecrod.PAR_PROCESS_ID, dbId).applyTo(dbDataset);
+
+			NumericPairList<Long, Long> numWaitsSeries = getNumWaitsTimeseries(tmpDataset);
+
+			NumericPairList<Long, Long> waitTimeSeries = getWaitTimeTimeseries(tmpDataset);
+			if (numWaitsSeries.size() != waitTimeSeries.size()) {
+				throw new RuntimeException("Unequal list sizes!");
+			}
+			List<Double> waitTimesPerLock = new ArrayList<>();
+			for (int i = 1; i < numWaitsSeries.size(); i++) {
+				long numWait_prev = numWaitsSeries.get(i - 1).getValue();
+				long waitTime_prev = waitTimeSeries.get(i - 1).getValue();
+				long numWait = numWaitsSeries.get(i).getValue();
+				long waitTime = waitTimeSeries.get(i).getValue();
+
+				waitTimesPerLock.add(((double) (waitTime - waitTime_prev) / ((double) (numWait - numWait_prev))));
+
+			}
+
+			if (prevNumUsers > 0) {
+				List<Double> sums1 = new ArrayList<>();
+				List<Double> sums2 = new ArrayList<>();
+				LpeNumericUtils.createNormalDistributionByBootstrapping(waitTimesPerLock_prev, waitTimesPerLock, sums1,
+						sums2);
+
+				if (sums2.size() < 2 || sums1.size() < 2) {
+					throw new IllegalArgumentException("too small sets");
+				}
+				double prevMean = LpeNumericUtils.average(sums1);
+				double currentMean = LpeNumericUtils.average(sums2);
+
+				double pValue = LpeNumericUtils.tTest(sums2, sums1);
+				if (pValue >= 0 && pValue <= requiredSignificanceLevel && prevMean < currentMean) {
+					if (firstSignificantNumUsers < 0) {
+						firstSignificantNumUsers = prevNumUsers;
+					}
+					significantSteps++;
+				} else {
+					firstSignificantNumUsers = -1;
+					significantSteps = 0;
+				}
+
+				// update chart data
+				if (prevNumUsers == minNumUsers) {
+					double stdDev = LpeNumericUtils.stdDev(sums1);
+					for (Double val : sums1) {
+						rawData.add(prevNumUsers, val);
+					}
+					double ciWidth = LpeNumericUtils.getConfidenceIntervalWidth(sums1.size(), stdDev,
+							requiredSignificanceLevel);
+					means.add(prevNumUsers, prevMean);
+					ci.add(ciWidth / 2.0);
+				}
+
+				double stdDev = LpeNumericUtils.stdDev(sums2);
+				for (Double val : sums2) {
+					rawData.add(numUsers, val);
+				}
+				double ciWidth = LpeNumericUtils.getConfidenceIntervalWidth(sums2.size(), stdDev,
+						requiredSignificanceLevel);
+				means.add(numUsers, currentMean);
+				ci.add(ciWidth / 2.0);
+			}
+			waitTimesPerLock_prev = waitTimesPerLock;
+			prevNumUsers = numUsers;
+		}
+
+		AnalysisChartBuilder chartBuilder = new AnalysisChartBuilder();
+		chartBuilder.startChart(dbId, "Num Users", "Avg. LockTime [ms]");
+		chartBuilder.addScatterSeries(rawData, "Lock Times");
+		getResultManager().storeImageChartResource(chartBuilder.build(), "Lock Times", result);
+
+		chartBuilder = new AnalysisChartBuilder();
+		chartBuilder.startChart(dbId, "Num Users", "CI: Lock Times [ms]");
+		chartBuilder.addScatterSeriesWithErrorBars(means, ci, "Lock Times");
+		getResultManager().storeImageChartResource(chartBuilder.build(), "Confidence Intervals", result);
+
+		if (firstSignificantNumUsers > 0 && significantSteps >= requiredSignificantSteps) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public InstrumentationDescription getInstrumentationDescription() {
+		InstrumentationDescriptionBuilder descrBuilder = new InstrumentationDescriptionBuilder();
+		descrBuilder.newSampling(CPUSampler.class.getName(), 50);
+		return descrBuilder.build();
+	}
+
+	private NumericPairList<Long, Long> getNumWaitsTimeseries(Dataset rtDataSet) {
+		NumericPairList<Long, Long> timeSeries = new NumericPairList<>();
+		for (DBStatisticsRecrod rec : rtDataSet.getRecords(DBStatisticsRecrod.class)) {
+			timeSeries.add(rec.getTimeStamp(), rec.getNumLockWaits());
+		}
+		timeSeries.sort();
+		return timeSeries;
+	}
+
+	private NumericPairList<Long, Long> getWaitTimeTimeseries(Dataset rtDataSet) {
+		NumericPairList<Long, Long> timeSeries = new NumericPairList<>();
+		for (DBStatisticsRecrod rec : rtDataSet.getRecords(DBStatisticsRecrod.class)) {
+			timeSeries.add(rec.getTimeStamp(), rec.getLockTime());
+		}
+
+		timeSeries.sort();
+		return timeSeries;
+	}
+
+	private Map<String, Integer> getNumberOfCPUCores(Dataset cpuUtilDataset) {
+		Map<String, Integer> cpuNumCores = new HashMap<>();
+		for (String processID : cpuUtilDataset.getValueSet(CPUUtilizationRecord.PAR_PROCESS_ID, String.class)) {
+			ParameterSelection selection = new ParameterSelection().select(CPUUtilizationRecord.PAR_PROCESS_ID,
+					processID);
+			int numCores = selection.applyTo(cpuUtilDataset).getValueSet(CPUUtilizationRecord.PAR_CPU_ID).size() - 1;
+			cpuNumCores.put(processID, numCores);
+		}
+		return cpuNumCores;
+	}
+}
---
+++ org.spotter.ext.detection.collection/src/org/spotter/ext/detection/dbcongestion/DBCongestionExtension.java
@@ -0,0 +1,118 @@
+/**
+ * Copyright 2014 SAP AG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.spotter.ext.detection.dbcongestion;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.lpe.common.config.ConfigParameterDescription;
+import org.lpe.common.util.LpeSupportedTypes;
+import org.spotter.core.detection.AbstractDetectionExtension;
+import org.spotter.core.detection.IDetectionController;
+
+public class DBCongestionExtension extends AbstractDetectionExtension {
+
+	// TODO: please provide a description
+	private static final String EXTENSION_DESCRIPTION = "no description";
+
+	
+	protected static final String DETECTION_STRATEGY_KEY = "strategy";
+	protected static final String THRESHOLD_STRATEGY = "fix threshold strategy";
+	protected static final String QT_STRATEGY = "queueing theory strategy";
+	public static final String REQUIRED_CONFIDENCE_LEVEL_KEY = "confidenceLevel";
+	public static final String REQUIRED_SIGNIFICANT_STEPS_KEY = "numSignificantSteps";
+	public static final String CPU_THRESHOLD_KEY = "cpuThreshold";
+	public static final String EXPERIMENT_STEPS_KEY = "numExperiments";
+
+	public static final double REQUIRED_CONFIDENCE_LEVEL_DEFAULT = 0.95;
+	public static final double CPU_THRESHOLD_DEFAULT = 0.90;
+	public static final int REQUIRED_SIGNIFICANT_STEPS_DEFAULT = 2;
+	public static final int EXPERIMENT_STEPS_DEFAULT = 4;
+
+	public IDetectionController createExtensionArtifact() {
+		return new DBCongestionDetectionController(this);
+	}
+
+	public String getName() {
+		return "DBO Congestion";
+	}
+
+	private ConfigParameterDescription createNumExperimentsParameter() {
+		ConfigParameterDescription numExperimentsParameter = new ConfigParameterDescription(EXPERIMENT_STEPS_KEY,
+				LpeSupportedTypes.Integer);
+		numExperimentsParameter.setDefaultValue(String.valueOf(EXPERIMENT_STEPS_DEFAULT));
+		numExperimentsParameter.setRange(String.valueOf(2), String.valueOf(Integer.MAX_VALUE));
+		numExperimentsParameter.setDescription("Number of experiments to execute with "
+				+ "different number of users between 1 and max number of users.");
+		return numExperimentsParameter;
+	}
+
+	private ConfigParameterDescription createNumSignificantStepsParameter() {
+		ConfigParameterDescription numSignificantStepsParameter = new ConfigParameterDescription(
+				REQUIRED_SIGNIFICANT_STEPS_KEY, LpeSupportedTypes.Integer);
+		numSignificantStepsParameter.setDefaultValue(String.valueOf(REQUIRED_SIGNIFICANT_STEPS_DEFAULT));
+		numSignificantStepsParameter.setRange(String.valueOf(1), String.valueOf(Integer.MAX_VALUE));
+		numSignificantStepsParameter.setDescription("This parameter specifies the number of steps between experiments "
+				+ "required to show a significant increase in order to detect a One Lane Bridge.");
+		return numSignificantStepsParameter;
+	}
+
+	private ConfigParameterDescription createConfidenceLevelParameter() {
+		ConfigParameterDescription requiredConfidenceLevel = new ConfigParameterDescription(
+				REQUIRED_CONFIDENCE_LEVEL_KEY, LpeSupportedTypes.Double);
+		requiredConfidenceLevel.setDefaultValue(String.valueOf(REQUIRED_CONFIDENCE_LEVEL_DEFAULT));
+		requiredConfidenceLevel.setRange("0.0", "1.0");
+		requiredConfidenceLevel.setDescription("This parameter defines the confidence level to be reached "
+				+ "in order to recognize a significant difference when comparing "
+				+ "two response time samples with the t-test.");
+		return requiredConfidenceLevel;
+	}
+
+	private ConfigParameterDescription createCPUThresholdParameter() {
+		ConfigParameterDescription requiredConfidenceLevel = new ConfigParameterDescription(CPU_THRESHOLD_KEY,
+				LpeSupportedTypes.Double);
+		requiredConfidenceLevel.setDefaultValue(String.valueOf(CPU_THRESHOLD_DEFAULT));
+		requiredConfidenceLevel.setRange("0.0", "1.0");
+		requiredConfidenceLevel
+				.setDescription("Fix threshold strategy ONLY! This parameter defines a threshold for the DB CPU to be considered as highly utilized");
+		return requiredConfidenceLevel;
+	}
+	
+	private ConfigParameterDescription createStrategyParameter() {
+		ConfigParameterDescription scopeParameter = new ConfigParameterDescription(DETECTION_STRATEGY_KEY,
+				LpeSupportedTypes.String);
+
+		Set<String> scopeOptions = new HashSet<>();
+		scopeOptions.add(QT_STRATEGY);
+		scopeOptions.add(THRESHOLD_STRATEGY);
+		scopeParameter.setOptions(scopeOptions);
+		scopeParameter.setDefaultValue(QT_STRATEGY);
+		scopeParameter.setDescription("This parameter determines the strategy, "
+				+ "used to analyse the Database congestion anti-pattern.");
+		return scopeParameter;
+	}
+
+	@Override
+	protected void initializeConfigurationParameters() {
+		addConfigParameter(ConfigParameterDescription.createExtensionDescription(EXTENSION_DESCRIPTION));
+		addConfigParameter(createConfidenceLevelParameter());
+		addConfigParameter(createNumSignificantStepsParameter());
+		addConfigParameter(createNumExperimentsParameter());
+		addConfigParameter(createCPUThresholdParameter());
+		addConfigParameter(createStrategyParameter());
+	}
+
+}
